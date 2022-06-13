6bc7ba09099b42fc976f520c02d008b600ad2b25
bergantingiulia <107368164+bergantingiulia@users.noreply.github.com>
2022-06-13 10:24:55 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,33 @@
+
+//pulsanti per la navigazione del sito
+
+funzione  OffertaSu ( )  {
+    finestra . scrollTo ( 0 ,  1820 ) ;
+  }
+
+  funzione  DurataSu ( )  {
+    finestra . scorrere verso ( 0 ,  3300 ) ;
+  }
+
+  funzione  Borse Su ( )  {
+    finestra . scorrere verso ( 0 ,  3800 ) ;
+  }
+
+  //bottoncino per andare su
+
+  funzione  scrollToTop ( )  {
+    finestra . scorrere verso ( 0 ,  0 ) ;
+  }
+
+  //apparire
+
+lascia che  popUp  =
+documento . getElementById ( "popup" ) ;
+
+funzione  vaiSu ( )  {
+    popup . style  =  "fondo: -50px;" ;
+}
+
+funzione  scompare ( )  {
+  popup . style  =  "visualizza: nessuno;" ;
+}
