f5c09d66e67d01f94308487130e10df6389e8b12
bergantingiulia <107368164+bergantingiulia@users.noreply.github.com>
2022-06-21 13:53:33 +0200

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function OffertaUp() {
   }
 
   function DurataUp() {
-    window.scrollTo(0, 3000);
+    window.scrollTo(0, 3100);
   }
 
   function BorseUp() {
@@ -17,22 +17,3 @@ function OffertaUp() {
   function scrollToTop() {
     window.scrollTo(0, 0);
   }
-
-    function showCookie() {
-      let cookie = document.querySelector('#cookie');
-      cookie.style = "display:block;";
-    }
-
-
-  //pop up
-
-let popUp =
-document.getElementById("popup");
-
-function goUp() {
-    popUp.style = "bottom: -50px;";
-}
-
-function disappear() {
-  popUp.style = "display: none;";
-}
