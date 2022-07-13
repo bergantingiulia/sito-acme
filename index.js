468e07559a36781088a147dbfda687144dc884c4
bergantingiulia <107368164+bergantingiulia@users.noreply.github.com>
2022-07-13 11:55:53 +0200

    Update index.js

--- index.js
+++ index.js
@@ -17,3 +17,13 @@ function OffertaUp() {
   function scrollToTop() {
     window.scrollTo(0, 0);
   }
+
+//cookie
+function showCookie() {
+      let cookie = document.querySelector('#cookie');
+      cookie.style = "display:block;";
+    }
+function hideCookie() {
+      let cookie = document.querySelector('#cookie');
+      cookie.style = "display:none;";
+    }
